f96a1c48d1ffc34f2bbdc5097022b82ea073a428
Cloudy-web20 <tanishkatailwal70@gmail.com>
2025-09-04 00:03:05 +0530

    Add files via upload

---
+++ array/array.js
@@ -0,0 +1,13 @@
+const skills = [
+{ name: "HTML", proficiency: "Intermediate" },
+{ name: "CSS", proficiency: "Advanced" },
+{ name: "JavaScript", proficiency: "Beginner" }
+];
+function formatSkills(skillsArray) {
+return skillsArray.map(skill => {
+return "${skill.name} (${skill.proficiency})";
+});
+}
+const formattedSkills = formatSkills(skills);
+console.log(formattedSkills);
+
