2277f1c17213ad37268be0ec691bfc78b0e2d960
Cloudy-web20 <tanishkatailwal70@gmail.com>
2025-09-04 23:31:00 +0530

    Delete toggle directory

--- toggle/toggle.js
+++
@@ -1,6 +0,0 @@
-const card = document.getElementById("card");
-const toggleBtn = document.getElementById("toggle-btn");
-
-toggleBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-});
